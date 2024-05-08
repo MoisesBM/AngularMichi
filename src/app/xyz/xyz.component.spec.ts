7a5c841a5b62d1adf82e8ae559ebea2fbf106c60
Milo443 <44039979+MoisesBM@users.noreply.github.com>
2024-05-08 11:18:08 -0500

    SDD

---
+++ src/app/xyz/xyz.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { XyzComponent } from './xyz.component';
+
+describe('XyzComponent', () => {
+  let component: XyzComponent;
+  let fixture: ComponentFixture<XyzComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [XyzComponent]
+    })
+    .compileComponents();
+    //SD
+    
+    fixture = TestBed.createComponent(XyzComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    //DSDSDSSD
+  });
+});
